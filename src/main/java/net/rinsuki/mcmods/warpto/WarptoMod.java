80128780a4ab2ee1d2ebde0a6b3b5a0e7cd68d10
rinsuki <428rinsuki+git@gmail.com>
2020-02-17 11:37:19 +0900

    Bump to 1.0.2

--- src/main/java/net/rinsuki/mcmods/warpto/WarptoMod.java
+++ src/main/java/net/rinsuki/mcmods/warpto/WarptoMod.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 public class WarptoMod {
     public static final String MODID = "warpto";
     public static final String NAME = "Warp Command";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     @EventHandler
     public void serverStarting(FMLServerStartingEvent event) {
